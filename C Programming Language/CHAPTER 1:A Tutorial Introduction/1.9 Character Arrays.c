e62eb809e3125e0a5a9158738fd72c8527be09ff
Chang9601 <79137839+Chang9601@users.noreply.github.com>
2021-03-01 13:49:24 +0900

    Create 1.9 Character Arrays.c

---
+++ C Programming Language/CHAPTER 1:A Tutorial Introduction/1.9 Character Arrays.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define MAX 1000
+#define LIMIT 80
+
+int getline(char s[],int maxline);
+
+int main()
+{
+    int c,len;
+    len = 0;
+
+    char line[MAX];
+    while((len = getline(line,MAX)) > 0)
+    {
+        if(len > LIMIT) printf("%s",line);
+    }
+}
+
+int getline(char s[],int lim)
+{
+    int c,i;
+
+    for(i = 0; i < lim-2 && (c = getchar()) != EOF && c != '\n'; ++i) s[i] = c;
+    if(c == '\n') s[i++] = c;
+    s[i] = '\0';
+    return i;
+}
