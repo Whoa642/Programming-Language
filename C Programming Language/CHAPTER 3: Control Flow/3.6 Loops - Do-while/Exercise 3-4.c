fd2e91d297215ebc86b7644cd1db8ce1fcb99fd2
Chang9601 <79137839+Chang9601@users.noreply.github.com>
2021-03-19 13:26:19 +0900

    Update Exercise 3-4.c

--- C Programming Language/CHAPTER 3: Control Flow/3.6 Loops - Do-while/Exercise 3-4.c
+++ C Programming Language/CHAPTER 3: Control Flow/3.6 Loops - Do-while/Exercise 3-4.c
@@ -1,8 +1,20 @@
 #define abs(x) ((x) > 0 ? (x) : (-x))
 
+void reverse(char s[])
+{
+    int i,j;
+    for(i = 0, j = strlen(s)-1; i < j; i++,j--)
+    {
+        char t = s[i];
+        s[i] = s[j];
+        s[j] = t;
+    }
+}
+
 void itoa(int n,char s[])
 {
     int i,sign;
+
     sign = n; i = 0; 
 
     do
