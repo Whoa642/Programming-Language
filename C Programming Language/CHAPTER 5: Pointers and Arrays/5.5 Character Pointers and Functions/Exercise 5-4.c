542838a6c6484254c39971a575be2d123eae9d79
Chang9601 <79137839+Chang9601@users.noreply.github.com>
2021-04-06 11:09:10 +0900

    Create Exercise 5-4.c

---
+++ C Programming Language/CHAPTER 5: Pointers and Arrays/5.5 Character Pointers and Functions/Exercise 5-4.c
@@ -0,0 +1,15 @@
+int strend(char *s, char *t)
+{
+    char *ps = s;
+    char *pt = t;
+
+    while(*s) s++;
+    while(*t) t++;
+
+    for(; *s == *t; s--, t--)
+        if(ps == s || pt == t)
+            break;
+    if(*s == *t && pt == t)
+        return 1;
+    return 0;
+}
