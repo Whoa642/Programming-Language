3969fbd563371eb2c0bdbcf50e7b012a437f6bf2
Chang9601 <79137839+Chang9601@users.noreply.github.com>
2021-04-28 22:10:20 +0900

    Update Exercise 7-1.c

--- C Programming Language/CHAPTER 7: Input and Output/7.1 Standard Input and Output/Exercise 7-1.c
+++ C Programming Language/CHAPTER 7: Input and Output/7.1 Standard Input and Output/Exercise 7-1.c
@@ -4,7 +4,7 @@
 int main(int argc,char *argv[])
 {
     int c;
-    if(strcmp(argv[0],"lower"))
+    if(strcmp(argv[0],"lower") == 0)
     {
         while((c = getchar()) != EOF)
             putchar(tolower(c));
