3522d24e76ae2cf29fe43bc05dfc737c9b1de947
Chang9601 <79137839+Chang9601@users.noreply.github.com>
2021-05-01 14:05:51 +0900

    Create Exercise 7-3.c

---
+++ C Programming Language/CHAPTER 7: Input and Output/7.1 Standard Input and Output/7.3 Variable-length Argument Lists/Exercise 7-3.c
@@ -0,0 +1,49 @@
+#include <stdarg.h>
+
+void minprintf(char *fmt, ...)
+{
+    va_list ap; 
+    char *p,*sval;
+    char cval;
+    int ival;
+    double dval;
+    void *pval;
+
+    va_start(ap,fmt); 
+    for(p = fmt; *p; p++)
+    {
+        if(*p != '%')
+        {
+            putchar(*p);
+            continue;
+        }
+
+        switch(*++p)
+        {
+            case 'c':
+                cval = va_arg(ap,int);
+                printf("%c",(char)cval);
+                break;
+            case 'i':
+            case 'd':
+                ival = va_arg(ap,int);
+                printf("%d",ival);
+                break;
+            case 'f':
+                dval = va_arg(ap,double);
+                printf("%f",dval);
+                break;
+            case 's':
+                for(sval = va_arg(ap,char*); *sval; sval++)
+                    putchar(*sval);
+                break;
+            case 'p':
+                pval = va_arg(ap,void*);
+                printf("%p",pval);
+            default:
+                putchar(*p);
+                break;
+        }
+        va_end(ap); 
+    }
+}
