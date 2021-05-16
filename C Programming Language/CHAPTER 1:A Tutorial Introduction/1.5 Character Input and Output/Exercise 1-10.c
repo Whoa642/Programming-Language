759f96537e31b89a33572f9439a01dc7e77f878e
Chang9601 <79137839+Chang9601@users.noreply.github.com>
2021-05-16 20:33:11 +0900

    Update Exercise 1-10.c

--- C Programming Language/CHAPTER 1:A Tutorial Introduction/1.5 Character Input and Output/Exercise 1-10.c
+++ C Programming Language/CHAPTER 1:A Tutorial Introduction/1.5 Character Input and Output/Exercise 1-10.c
@@ -5,10 +5,14 @@ int main()
     int c;
     while((c = getchar()) != EOF)
     {
-        if(c == '\t') printf("\\t");
-        else if(c == '\b') printf("\\b");
-        else if(c == '\\') printf("\\\\");
-        else putchar(c);
+        if(c == '\t') 
+            printf("\\t");
+        if(c == '\b') 
+            printf("\\b");
+        if(c == '\\') 
+            printf("\\\\");
+        if(c != '\t' && c != '\b' && c != '\\')
+            putchar(c);
     }
     return 0;
 }
