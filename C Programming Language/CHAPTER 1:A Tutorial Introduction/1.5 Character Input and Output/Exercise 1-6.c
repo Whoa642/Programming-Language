9b7d3c7097eb2969f34ccc22e28ba674b57642f9
Chang9601 <79137839+Chang9601@users.noreply.github.com>
2021-05-16 19:37:55 +0900

    Update Exercise 1-6.c

--- C Programming Language/CHAPTER 1:A Tutorial Introduction/1.5 Character Input and Output/Exercise 1-6.c
+++ C Programming Language/CHAPTER 1:A Tutorial Introduction/1.5 Character Input and Output/Exercise 1-6.c
@@ -3,7 +3,7 @@
 int main()
 {
     int c;
-    while(c = getchar != EOF) // 0 if c returns EOF or 1 if not
+    while(c = getchar != EOF) // c is assigned 0 if getchar() returns EOF or 1 if not
         printf("%d\n",c);
     
     return 0;
