70a2e7d39145d6112a9598b1cf3fdb5e61819db5
Chang9601 <79137839+Chang9601@users.noreply.github.com>
2021-05-16 19:35:37 +0900

    Update Exercise 1-6.c

--- C Programming Language/CHAPTER 1:A Tutorial Introduction/1.5 Character Input and Output/Exercise 1-6.c
+++ C Programming Language/CHAPTER 1:A Tutorial Introduction/1.5 Character Input and Output/Exercise 1-6.c
@@ -3,8 +3,8 @@
 int main()
 {
     int c;
-    while(c = getchar != EOF) // infinite loop, so the value is 1
-        printf("No");
+    while(c = getchar != EOF) // 0 if c returns EOF or 1 if not
+        printf("%d\n",c);
     
     return 0;
 }
