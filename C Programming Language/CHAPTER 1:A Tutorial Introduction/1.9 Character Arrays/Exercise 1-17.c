eeeb415685b910d3a709671e85c6ef891043500e
Chang9601 <79137839+Chang9601@users.noreply.github.com>
2021-05-24 20:47:15 +0900

    Update Exercise 1-17.c

--- C Programming Language/CHAPTER 1:A Tutorial Introduction/1.9 Character Arrays/Exercise 1-17.c
+++ C Programming Language/CHAPTER 1:A Tutorial Introduction/1.9 Character Arrays/Exercise 1-17.c
@@ -4,22 +4,26 @@
 
 int getline(char s[],int maxline);
 
-int main()
+int main(void)
 {
-    int c,len;
-    len = 0;
-
+    int len;
     char line[MAX];
+
+    len = 0;
     while((len = getline(line,MAX)) > 0)
-        if(len > LIMIT) printf("%s",line);
+        if(len > LIMIT) 
+            printf("%s",line);
 }
 
 int getline(char s[],int lim)
 {
     int c,i;
 
-    for(i = 0; i < lim-2 && (c = getchar()) != EOF && c != '\n'; ++i) s[i] = c;
-    if(c == '\n') s[i++] = c;
+    for(i = 0; i < lim-2 && (c = getchar()) != EOF && c != '\n'; i++) 
+        s[i] = c;
+
+    if(c == '\n') 
+        s[i++] = c;
     s[i] = '\0';
     return i;
 }
